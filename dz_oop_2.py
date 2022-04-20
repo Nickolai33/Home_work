e0426c6b97b94e9ce6d80a8420878885bccb61c9
Nickolai <kosmosggg@gmail.com>
2022-04-20 20:19:35 +0300

    Done!

--- dz_oop_2.py
+++ dz_oop_2.py
@@ -4,7 +4,7 @@
 #    1) lang - язык и 2) letters - список букв. Начальные значения свойств берутся из входных параметров метода.
 # 3. Создайте метод print(), который выведет в консоль буквы алфавита.
 # 4. Создайте метод letters_num(), который вернет количество букв в алфавите.
-#
+
 # Класс EngAlphabet
 # 1. Создайте класс EngAlphabet путем наследования от класса Alphabet.
 # 2. Создайте метод __init__(), внутри которого будет вызываться родительский метод __init__().
@@ -16,6 +16,14 @@
 # 5. Переопределите метод letters_num() - пусть в текущем классе он будет возвращать значение свойства __letters_num.
 # 6. Создайте статический метод example(), который будет возвращать пример текста на английском языке.
 
+# Тесты:
+# 1. Создайте объект класса EngAlphabet
+# 2. Напечатайте буквы алфавита для этого объекта
+# 3. Выведите количество букв в алфавите
+# 4. Проверьте, относится ли буква F к английскому алфавиту
+# 5. Проверьте, относится ли буква Щ к английскому алфавиту
+# 6. Выведите пример текста на английском языке
+
 from string import ascii_uppercase
 
 
@@ -25,7 +33,6 @@ class Alphabet:
         self.lang = lang
         self.letters = letters
 
-
     def print(self):
         return print(self.letters)
 
@@ -34,6 +41,7 @@ class Alphabet:
 
 
 class EngAlphabet(Alphabet):
+    __letters_num = 26
 
     def __init__(self):
         super().__init__(lang='En', letters=ascii_uppercase)
@@ -41,7 +49,22 @@ class EngAlphabet(Alphabet):
 
     def is_en_letter(self, a):
         if a in self.letters:
-            print('This letter is the English!')
+            print(f'This letter {a} is the English!')
         else:
-            print("This letter isn't English!")
+            print(f"This letter {a} isn't English(((")
 
+    def letters_num(self):
+        return EngAlphabet.__letters_num
+
+    @staticmethod
+    def example():
+        print('Sample text in english.')
+
+
+if __name__ == '__main__':
+    eng = EngAlphabet()
+    eng.print()
+    print(eng.letters_num())
+    eng.is_en_letter('D')
+    eng.is_en_letter('Э')
+    eng.example()
